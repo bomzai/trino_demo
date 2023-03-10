1d8ffbbb228c5b2415d204b2b25d647fc5787380
Nazano <antoninus.antoninus@me.com>
2023-03-10 18:33:52 +0100

    feat(scripts): bind env variable to container scripts

--- scripts/mysql_import.sh
+++ scripts/mysql_import.sh
@@ -1,21 +1,8 @@
-MYSQL_USER='root'
-MYSQL_PASS='root'
-SHARED_VOLUME_PATH="/data"
-EXPORT_PATH="${SHARED_VOLUME_PATH}/export"
-MYSQL_DATA="${EXPORT_PATH}/mysql"
-ZIP_MYSQL_DATA_PATH="./${MYSQL_DATA}/export_mysql.tar"
-
-DATABASE="films"
-BASICS_TABLE="basics"
-BASIC_TABLE_FILE="old_basics_table.tsv"
-RATINGS_TABLE="ratings"
-RATINGS_TABLE_FILE="old_ratings_table.tsv"
-DATES_TABLE="dates"
-DATES_TABLE_FILE="old_titles_dates.tsv"
+# Load data into MySQL
 
 # Wait for mysqld to be initalized
 echo "Waiting for mysqld"
-while ! mysqladmin ping -h localhost --user=${MYSQL_USER} --password=${MYSQL_PASS} 2> /dev/null; do
+while ! mysqladmin ping -h localhost --user=${MYSQL_CUSTOM_USER} --password=${MYSQL_PASSWORD} 2> /dev/null; do
     echo "..."
     sleep 1
 done
@@ -23,26 +10,26 @@ echo -e "\nmysqld ready !"
 
 # Wait for the data
 echo "Waiting for data"
-while [ ! -f "${MYSQL_DATA}/EOF" ]; do sleep 1; done
-echo "Zipepd data uploaded"
+while [ ! -f "${PYTHON_MYSQL_EOF}" ]; do sleep 1; done
+echo "Zipped data uploaded"
 
 # Unzip data
-chmod 755 "/${EXPORT_PATH}"/*
-tar -xvf "${ZIP_MYSQL_DATA_PATH}" -C "${MYSQL_DATA}"
+tar -xvf "${MYSQL_ZIP_DATA_PATH}.tar" -C "${MYSQL_DATA_PATH}"
 
-IMPORT_REQUEST="""LOAD DATA INFILE '/${MYSQL_DATA}/${DATES_TABLE_FILE}'
+IMPORT_REQUEST="""LOAD DATA INFILE '${MYSQL_DATA_PATH}/${DATES_TABLE_FILE}'
 INTO TABLE ${DATES_TABLE}
 IGNORE 1 ROWS
 SET dataInsertTime = str_to_date(dataInsertTime, '%Y-%m-%d');
 
-LOAD DATA INFILE '/${MYSQL_DATA}/${BASIC_TABLE_FILE}'
+LOAD DATA INFILE '${MYSQL_DATA_PATH}/${BASIC_TABLE_FILE}'
 INTO TABLE ${BASICS_TABLE}
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/${MYSQL_DATA}/${RATINGS_TABLE_FILE}'
+LOAD DATA INFILE '${MYSQL_DATA_PATH}/${RATINGS_TABLE_FILE}'
 INTO TABLE ${RATINGS_TABLE}
 IGNORE 1 ROWS;
 """
 
 # Load data into mysql
-mysql -D ${DATABASE} --user=${MYSQL_USER} --password=${MYSQL_PASS} -e "${IMPORT_REQUEST}"
+mysql -D ${DATABASE} --user=${MYSQL_CUSTOM_USER} --password=${MYSQL_PASSWORD} -e "${IMPORT_REQUEST}"
+echo "Data loaded into ${DATABASE}"
--- scripts/zip_data.py
+++ scripts/zip_data.py
@@ -1,6 +1,5 @@
 """Process tables and zip it to data folder
 """
-
 import pandas as pd
 from os.path import join, abspath
 import functools as ft
@@ -9,77 +8,70 @@ import os
 import sys
 
 ROOT_PATH = abspath(join(__file__ , "../.."))
-MYSQL_TABLES_NAMES = ["old_basics_table", "old_ratings_table", "old_titles_dates"]
-MONGODB_TABLES_NAMES = ["recent_titles_dates", "recent_basics_table", "recent_ratings_table"]
-BASICS_COLUMNS = ["tconst", "titleType", "primaryTitle", "originalTitle", "isAdult", "startYear", "endYear", "runtimeMinutes", "genres"]
-RATINGS_COLUMNS = ["tconst", "averageRating", "numVotes"]
-DATES_COLUMNS = ["tconst", "dataInsertTime"]
-FILE_FORMAT = "tsv"
-INSERTION_DATE = "2023-01-01"
+MYSQL_TABLES_NAMES=["old_basics_table", "old_ratings_table", "old_titles_dates"]
+MONGODB_TABLES_NAMES=["recent_titles_dates", "recent_basics_table", "recent_ratings_table"]
+BASICS_COLUMNS=["tconst", "titleType", "primaryTitle", "originalTitle", "isAdult", "startYear", "endYear", "runtimeMinutes", "genres"]
+RATINGS_COLUMNS=["tconst", "averageRating", "numVotes"]
+DATES_COLUMNS=["tconst", "dataInsertTime"]
 
 def read_data():
     """Read dataframes in data folder
-    
     Returns:
         pd.DataFrame: Return splitted dataset for mongo and mysql.
     """
-    title_basics = pd.read_csv(join(ROOT_PATH, "title_basics_truncated.tsv"), sep="\t")
-    title_ratings = pd.read_csv(join(ROOT_PATH, "title_ratings_truncated.tsv"), sep="\t")
-    title_dates = pd.read_csv(join(ROOT_PATH, "data_insert_time.tsv"), sep="\t")
-
+    title_basics = pd.read_csv(join(os.environ["EXPORT_PATH"], os.environ["BASICS_TABLE_UNPROCESS_FILE"]), sep="\t")
+    title_ratings = pd.read_csv(join(os.environ["EXPORT_PATH"], os.environ["RATINGS_TABLE_UNPROCESS_FILE"]), sep="\t")
+    title_dates = pd.read_csv(join(os.environ["EXPORT_PATH"], os.environ["DATES_TABLE_UNPROCESS_FILE"]), sep="\t")
     return title_basics, title_ratings, title_dates
 
 def process_data():
     """Chunk dataframes into 6 tables filtered on recent dates
-    
     Returns:
         pd.DataFrame
     """
     title_basics, title_ratings, title_dates = read_data()
-
     dfs = [title_basics, title_ratings, title_dates]
+
     # Join all tables to filter on the date
     titles = ft.reduce(lambda left, right: pd.merge(left, right, on='tconst'), dfs)
 
     # Then rebuild back the tables filtered on the date
-    old_basics_table = titles[titles["dataInsertTime"] < INSERTION_DATE][BASICS_COLUMNS]
-    old_ratings_table = titles[titles["dataInsertTime"] < INSERTION_DATE][RATINGS_COLUMNS]
-    old_titles_dates = titles[titles["dataInsertTime"] < INSERTION_DATE][DATES_COLUMNS]
-    recent_basics_table = titles[titles["dataInsertTime"] > INSERTION_DATE][BASICS_COLUMNS]
-    recent_ratings_table = titles[titles["dataInsertTime"] > INSERTION_DATE][RATINGS_COLUMNS]
-    recent_titles_dates = titles[titles["dataInsertTime"] > INSERTION_DATE][DATES_COLUMNS]
+    old_basics_table = titles[titles["dataInsertTime"] < os.environ["INSERTION_DATE"]][BASICS_COLUMNS]
+    old_ratings_table = titles[titles["dataInsertTime"] < os.environ["INSERTION_DATE"]][RATINGS_COLUMNS]
+    old_titles_dates = titles[titles["dataInsertTime"] < os.environ["INSERTION_DATE"]][DATES_COLUMNS]
+    recent_basics_table = titles[titles["dataInsertTime"] > os.environ["INSERTION_DATE"]][BASICS_COLUMNS]
+    recent_ratings_table = titles[titles["dataInsertTime"] > os.environ["INSERTION_DATE"]][RATINGS_COLUMNS]
+    recent_titles_dates = titles[titles["dataInsertTime"] > os.environ["INSERTION_DATE"]][DATES_COLUMNS]
 
     return old_basics_table, old_ratings_table, old_titles_dates, recent_titles_dates, recent_basics_table, recent_ratings_table
 
 def dataframe_to_tsv():
     """ Create export mysql and export mongodb folder, convert dataframes into tsv files in the export folder
     """
-
     old_basics_table, old_ratings_table, old_titles_dates, recent_titles_dates, recent_basics_table, recent_ratings_table = process_data()
-
     mysql_tables = [old_basics_table, old_ratings_table, old_titles_dates]
     mongodb_tables = [recent_titles_dates, recent_basics_table, recent_ratings_table]
 
-    os.makedirs(join(ROOT_PATH, "export/temp_mysql/"))
-    os.makedirs(join(ROOT_PATH,"export/temp_mongodb/"))
+    os.makedirs(os.environ["PYTHON_MYSQL_TEMP_FOLDER"])
+    os.makedirs(os.environ["PYTHON_MONGODB_TEMP_FOLDER"])
     print("Created folders")
 
     for table, table_name in zip(mysql_tables, MYSQL_TABLES_NAMES):
-        table.to_csv(join(ROOT_PATH, "export/temp_mysql/", table_name + "." + FILE_FORMAT), sep="\t", index=False)
-
+        table.to_csv(join(os.environ["PYTHON_MYSQL_TEMP_FOLDER"], table_name + "." + os.environ["FILE_FORMAT"]), sep="\t", index=False)
+    
     for table, table_name in zip(mongodb_tables, MONGODB_TABLES_NAMES):
-        table.to_csv(join(ROOT_PATH, "export/temp_mongodb/", table_name + "." + FILE_FORMAT), sep="\t", index=False)
+        table.to_csv(join(os.environ["PYTHON_MONGODB_TEMP_FOLDER"], table_name + "." + os.environ["FILE_FORMAT"]), sep="\t", index=False)
 
 def zip_data():
     """ Zip tsv files into export folder
     """
-    shutil.make_archive(join(ROOT_PATH, "export/mongodb/export_mongodb"), 'tar', join(ROOT_PATH, "export/temp_mongodb"))
-    shutil.make_archive(join(ROOT_PATH, "export/mysql/export_mysql"), 'tar', join(ROOT_PATH, "export/temp_mysql"))
+    shutil.make_archive(os.environ["MONGODB_ZIP_DATA_PATH"], 'tar', os.environ["PYTHON_MONGODB_TEMP_FOLDER"])
+    shutil.make_archive(os.environ["MYSQL_ZIP_DATA_PATH"], 'tar', os.environ["PYTHON_MYSQL_TEMP_FOLDER"])
     print("Archives done")
 
     # Create EOF file telling the data prep is done
-    open(join(ROOT_PATH, 'export/mysql/EOF'), 'a').close()
-    open(join(ROOT_PATH, 'export/mongodb/EOF'), 'a').close()
+    open(os.environ["PYTHON_MYSQL_EOF"], 'a').close()
+    open(os.environ["PYTHON_MONGODB_EOF"], 'a').close()
 
 if __name__ == '__main__':
     if sys.argv[1] == "docker":
@@ -89,7 +81,6 @@ if __name__ == '__main__':
     zip_data()
 
     # Delete useless folder
-    shutil.rmtree(join(ROOT_PATH, "export/temp_mongodb"))
-    shutil.rmtree(join(ROOT_PATH, "export/temp_mysql"))
-
+    shutil.rmtree(os.environ["PYTHON_MYSQL_TEMP_FOLDER"])
+    shutil.rmtree(os.environ["PYTHON_MONGODB_TEMP_FOLDER"])
     print("MongoDB and MySQL temp folders deleted")
